8735de7e54f96364a3a9dd8a5cfe780e9ab71c0d
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-21 22:50:54 +0100

    Testing services in angular.js

---
+++ spec/expensesControllerSpec.js
@@ -0,0 +1,18 @@
+describe('expensesController', function(){
+
+	var $controllerConstructor;
+	var scope;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function($controller, $rootScope){
+		$controllerConstructor = $controller;
+		scope = $rootScope.$new();
+	}));
+
+	it('should have three expense items', function(){
+		var ctrl = $controllerConstructor('expensesController', {$scope:scope});
+		console.log(ctrl.expenseItems.length);
+		expect(ctrl.expenseItems.length).toBe(3);
+	});
+});
---
+++ spec/expensesDataServiceSpec.js
@@ -0,0 +1,10 @@
+'use strict'
+describe('expensesDataService', function(){
+
+	beforeEach(module('app'));
+
+	it('should return three expense items', inject(function(expensesDataService){
+		expect(expensesDataService.getExpenses().length).toBe(3);
+	}));
+});
+
