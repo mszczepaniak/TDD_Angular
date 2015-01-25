d9ffa33584d36eb598a956973ea373ee6a06bbd6
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-25 13:26:37 +0100

    Add test to expenseDataService

--- spec/expensesDataServiceSpec.js
+++ spec/expensesDataServiceSpec.js
@@ -6,5 +6,12 @@ describe('expensesDataService', function(){
 	it('should return three expense items', inject(function(expensesDataService){
 		expect(expensesDataService.getExpenses().length).toBe(3);
 	}));
+
+	it('should return a taxi expense', inject(function(expensesDataService){
+		var expenseItems = expensesDataService.getExpenses();
+		var testExpenseItem = new ExpenseItem('Taxi','To airport', 89.95);
+		expect(expenseItems).toContain(testExpenseItem);
+	}))
+
 });
 
