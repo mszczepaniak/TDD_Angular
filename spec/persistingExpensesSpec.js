b9449eba290382fbea78723e58fed70cd2c023d8
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-01-26 08:40:08 +0100

    Add call fake tests

--- spec/persistingExpensesSpec.js
+++ spec/persistingExpensesSpec.js
@@ -18,6 +18,14 @@ describe('persisting expenses', function(){
 			var spy = spyOn(expensesDataService, 'persistExpenses');
 			expensesDataService.persistExpenses();
 			expect(spy).toHaveBeenCalled();
+		}));
+
+		it('should spy on persistExpenses and fake a reply', inject(function(expensesDataService){
+			var spy = spyOn(expensesDataService,'persistExpenses').and.callFake(function(){
+				return 3;
+			});
+			var numRecordsPersisted = expensesDataService.persistExpenses();
+			expect(numRecordsPersisted).toEqual(3);
 		}))
 	});
 });
